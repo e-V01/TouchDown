d7274e7084ad80c1a507fe078911d1f873bf3516
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-02-01 15:00:37 +0200

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Y K on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
