4f2d19c66d90320ccd91b92877bf48f4bc36c988
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-02-07 13:06:56 +0200

    Added to HomeView : LogoView & NavigationBarView Had to create a general padding with UIapplication shared window to fir all screen sizes

--- TouchDown/App/ContentView.swift
+++ TouchDown/App/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                // sets the unified space at the top for each size of screen
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                Spacer()
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
